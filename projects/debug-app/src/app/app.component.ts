31dadc1e6939b337dbaa0e8a7e5b6c1794e7e51f
orzirou <S.ohtawa0707@gmail.com>
2022-11-24 21:33:37 +0900

    Edit debug-app

--- projects/debug-app/src/app/app.component.ts
+++ projects/debug-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { CounterSpinnerDisplayMode } from '../../../ngx-mat-counterspinner/src/lib';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'debug-app';
+
+  readonly CounterSpinnerDisplayMode = CounterSpinnerDisplayMode;
 }
--- projects/debug-app/src/app/app.module.ts
+++ projects/debug-app/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { NgxMatCounterspinnerModule } from '../../../ngx-mat-counterspinner/src/lib';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, NgxMatCounterspinnerModule],
   providers: [],
   bootstrap: [AppComponent],
 })
